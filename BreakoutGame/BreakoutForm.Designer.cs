8784bff5ed1a70e272a7082e0cda240dc09dcd95
Rafał Czarzasty <rafal.czarzasty@gmail.com>
2017-09-09 20:49:35 +0200

    Breakout game completed

---
+++ BreakoutGame/BreakoutForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace BreakoutGame
+{
+    partial class BreakoutForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BreakoutForm));
+            this.picPaddle = new System.Windows.Forms.PictureBox();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnQuitGame = new System.Windows.Forms.Button();
+            this.btnResme = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.pnlMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picPaddle
+            // 
+            this.picPaddle.BackColor = System.Drawing.Color.Transparent;
+            this.picPaddle.Image = ((System.Drawing.Image)(resources.GetObject("picPaddle.Image")));
+            this.picPaddle.Location = new System.Drawing.Point(336, 526);
+            this.picPaddle.Margin = new System.Windows.Forms.Padding(5);
+            this.picPaddle.Name = "picPaddle";
+            this.picPaddle.Size = new System.Drawing.Size(116, 12);
+            this.picPaddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picPaddle.TabIndex = 0;
+            this.picPaddle.TabStop = false;
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.Transparent;
+            this.picBall.Image = ((System.Drawing.Image)(resources.GetObject("picBall.Image")));
+            this.picBall.Location = new System.Drawing.Point(376, 486);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(32, 32);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBall.TabIndex = 1;
+            this.picBall.TabStop = false;
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.BackColor = System.Drawing.Color.Transparent;
+            this.lblGameOver.Font = new System.Drawing.Font("Showcard Gothic", 36F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblGameOver.Location = new System.Drawing.Point(213, 13);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(364, 74);
+            this.lblGameOver.TabIndex = 2;
+            this.lblGameOver.Text = "Game Over";
+            this.lblGameOver.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "stone_block.png");
+            this.imageList1.Images.SetKeyName(1, "blue_block.png");
+            this.imageList1.Images.SetKeyName(2, "brown_block.png");
+            this.imageList1.Images.SetKeyName(3, "green_block.png");
+            this.imageList1.Images.SetKeyName(4, "pink_block.png");
+            this.imageList1.Images.SetKeyName(5, "red_block.png");
+            this.imageList1.Images.SetKeyName(6, "skyblue_block.png");
+            this.imageList1.Images.SetKeyName(7, "violet_block.png");
+            this.imageList1.Images.SetKeyName(8, "yellow_block.png");
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.Transparent;
+            this.pnlMenu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlMenu.BackgroundImage")));
+            this.pnlMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlMenu.Controls.Add(this.label1);
+            this.pnlMenu.Controls.Add(this.btnQuitGame);
+            this.pnlMenu.Controls.Add(this.btnResme);
+            this.pnlMenu.Controls.Add(this.btnNewGame);
+            this.pnlMenu.Location = new System.Drawing.Point(192, 166);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(400, 300);
+            this.pnlMenu.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(136, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MENU";
+            // 
+            // btnQuitGame
+            // 
+            this.btnQuitGame.BackColor = System.Drawing.Color.Gold;
+            this.btnQuitGame.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitGame.ForeColor = System.Drawing.Color.Tomato;
+            this.btnQuitGame.Location = new System.Drawing.Point(111, 162);
+            this.btnQuitGame.Name = "btnQuitGame";
+            this.btnQuitGame.Size = new System.Drawing.Size(200, 42);
+            this.btnQuitGame.TabIndex = 2;
+            this.btnQuitGame.Text = "QUIT GAME";
+            this.btnQuitGame.UseVisualStyleBackColor = false;
+            this.btnQuitGame.Click += new System.EventHandler(this.btnQuitGame_Click);
+            // 
+            // btnResme
+            // 
+            this.btnResme.BackColor = System.Drawing.Color.Gold;
+            this.btnResme.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResme.ForeColor = System.Drawing.Color.Tomato;
+            this.btnResme.Location = new System.Drawing.Point(111, 114);
+            this.btnResme.Name = "btnResme";
+            this.btnResme.Size = new System.Drawing.Size(200, 42);
+            this.btnResme.TabIndex = 1;
+            this.btnResme.Text = "Resume";
+            this.btnResme.UseVisualStyleBackColor = false;
+            this.btnResme.Click += new System.EventHandler(this.btnResme_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.Gold;
+            this.btnNewGame.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.ForeColor = System.Drawing.Color.Tomato;
+            this.btnNewGame.Location = new System.Drawing.Point(111, 66);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(200, 42);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "NEW GAME";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(13, 13);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(100, 35);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "00000";
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.BackColor = System.Drawing.Color.Transparent;
+            this.lblCountdown.Font = new System.Drawing.Font("Showcard Gothic", 48F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblCountdown.Location = new System.Drawing.Point(661, 146);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(82, 98);
+            this.lblCountdown.TabIndex = 5;
+            this.lblCountdown.Text = "3";
+            this.lblCountdown.Visible = false;
+            // 
+            // BreakoutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(782, 620);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pnlMenu);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.picBall);
+            this.Controls.Add(this.picPaddle);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "BreakoutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Breakout!";
+            this.Load += new System.EventHandler(this.BreakoutForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.BreakoutForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BreakoutForm_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BreakoutForm_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPaddle;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnQuitGame;
+        private System.Windows.Forms.Button btnResme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblCountdown;
+    }
+}
+
---
+++ BreakoutGame/BreakoutForm.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace BreakoutGame
+{
+    public partial class BreakoutForm : Form
+    {
+        //game default variables
+        SoundPlayer startSound = new SoundPlayer(BreakoutGame.Properties.Resources.start);
+        SoundPlayer quitSound = new SoundPlayer(Properties.Resources.quit);
+        SoundPlayer paddleTouchSound = new SoundPlayer(Properties.Resources.paddle_touch);
+        SoundPlayer countdownSound = new SoundPlayer(Properties.Resources.countdown);
+        SoundPlayer hit1Sound = new SoundPlayer(Properties.Resources.Hit1);
+        SoundPlayer speedSound = new SoundPlayer(Properties.Resources.speed);
+        Timer gameTimer = new Timer();
+        Random rand = new Random();
+        int paddleSpeed = 30;
+        bool gamePaused = true;
+        int score = 0;
+
+        //Ball variables
+        int ballSpeed = 5;
+        int ballDX = 1;
+        int ballDY = 1;
+
+        //Block variables
+        Image[,] Blocks;
+        int blocksRows;
+        int blocksCol;
+        int blocksCount= 0;
+
+        private bool IsPaused()
+        {
+            return gamePaused;
+        }
+
+        private void PauseGame(bool Pause = true)
+        {
+            ShowMenu(Pause);
+            gameTimer.Enabled = !Pause;
+            gamePaused = Pause;
+            btnResme.Enabled = Pause;
+        }
+        private void ShowMenu(bool Show = true)
+        {
+            pnlMenu.Visible = Show;
+            Invalidate();
+        }
+
+        public BreakoutForm()
+        {
+            InitializeComponent();
+        }
+
+        //Blocks with images
+        private int BuildBlocks(int rows, int cols)
+        {
+            blocksRows = rows;
+            blocksCol = cols;
+
+            Blocks = new Image[rows, cols];
+
+            for (int i = 0; i < rows; ++i)
+                for (int j = 0; j < cols; ++j)
+                {
+                    int index = rand.Next(0, imageList1.Images.Count);
+                    Blocks[i, j] = imageList1.Images[index];
+                    Blocks[i, j].Tag = index;
+                }
+            return rows * cols;
+        }
+
+        private void MovePaddle(int newXPos)
+        {
+            if (newXPos < 0)
+                newXPos = 0;
+            else if (newXPos > ClientRectangle.Width - picPaddle.Width)
+                newXPos = ClientRectangle.Width - picPaddle.Width;
+            picPaddle.Left = newXPos;
+        }
+
+        private void BreakoutForm_Load(object sender, EventArgs e)
+        {
+            //center game paddle
+            MovePaddle((ClientRectangle.Width - picPaddle.Width) / 2);
+            //center ball
+            picBall.Left = (ClientRectangle.Width - picBall.Width) / 2;
+            picBall.Top = 250;
+            //game timer
+            gameTimer.Interval = 1;
+            gameTimer.Tick += GameTimer_Tick;
+            //blocks
+            blocksCount = BuildBlocks(rand.Next(3, 5), imageList1.Images.Count);
+
+            //game will start paused
+            PauseGame(true);
+            //return disabled when starting game
+            btnResme.Enabled = false;
+            //play sound
+            startSound.Play();
+        }
+        private void ShowGameOver(string text = "Game Over")
+        {
+            lblGameOver.Text = text;
+            lblGameOver.Left = (ClientRectangle.Width - lblGameOver.Width) / 2;
+            lblGameOver.Top = 60;
+            lblGameOver.Visible = true;
+            gameTimer.Enabled = false;
+            quitSound.Play();
+            for (int i = 0; i < 10; ++i)
+            {
+                lblGameOver.Top += 20;
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(30);
+            }
+            System.Threading.Thread.Sleep(3000);
+            lblGameOver.Visible = false;
+            PauseGame(true);
+            btnResme.Enabled = false;
+
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            //ball movement
+            Point pt = picBall.Location;
+
+            pt.X += ballSpeed * ballDX;
+            pt.Y += ballSpeed * ballDY;
+            picBall.Location = pt;
+            if (pt.X < 0 || pt.X > ClientRectangle.Width - picBall.Width)
+                ballDX = -ballDX;
+            if (pt.Y < 0)
+                ballDY = -ballDY;
+            //game over
+            if (pt.Y > ClientRectangle.Height)
+            {
+                ShowGameOver();
+            }
+
+            //collision detect with paddle
+            if(picBall.Bounds.IntersectsWith(picPaddle.Bounds))
+            {
+                paddleTouchSound.Play();
+                ballDY = -ballDY;
+            }
+            //collision detect between ball/blocks
+            //first select all ball corners
+            Point[] pts = new Point[]
+            {
+                new Point (pt.X, pt.Y),
+                new Point (pt.X + picBall.Width, pt.Y),
+                new Point (pt.X, pt.Y + picBall.Height),
+                new Point (pt.X + picBall.Width, pt.Y + picBall.Height)
+            };
+            //then interact with blocks
+            int ballHitCount = 0;
+            foreach (Point ptBall in pts)
+            {
+                int imgWidth = imageList1.ImageSize.Width;
+                int imgHeight = imageList1.ImageSize.Height;
+                int xpos = xpos = (ClientRectangle.Width - imgWidth * blocksCol) / 2;
+                int ypos = 70;
+                int row = pt.Y - ypos;
+                int col = pt.X - xpos;
+                col /= imgWidth;
+                row /= imgHeight;
+
+                if (col >= 0 && col < blocksCol && row >= 0 && row < blocksRows)
+                {
+                    if (Blocks[row, col] != null)
+                    {
+                        if ((int)Blocks[row, col].Tag == 0)
+                        {
+                            //stone block incrase ball speed
+                            ballSpeed += 1;
+                            score += 5;
+                            if (ballHitCount == 0)
+                                speedSound.Play();
+                        }
+                        else if (ballHitCount == 0)
+                            hit1Sound.Play();
+                        Blocks[row, col] = null;
+                        Rectangle rc = new Rectangle(xpos + col * imgWidth, ypos + row * imgHeight, imgWidth, imgHeight);
+                        Invalidate(rc);
+                        ++ballHitCount;
+                    }
+                }
+            }
+            //if we have hit then ball will return
+            if (ballHitCount > 0)
+            {
+                ballDY = -ballDY;
+
+                score += ballHitCount;
+                lblScore.Text = score.ToString("D5");
+
+                blocksCount -= ballHitCount;
+                if (blocksCount <= 0)
+                {
+                    //level completed
+                    ShowGameOver("Level Completed!");
+                }
+            }
+
+        }
+
+        private void BreakoutForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if game is paused paddle movement is blocked
+            if (IsPaused())
+                return;
+            //paddle will move with mouse
+            MovePaddle(e.X);
+        }
+
+        private void BreakoutForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            //if game menu is on the screen don't show blocks
+            if (pnlMenu.Visible)
+                return;
+
+            int xpos;
+            int ypos = 70;
+            int imgWidth = imageList1.ImageSize.Width;
+            int imgHeighth = imageList1.ImageSize.Height;
+            for (int i = 0; i < blocksRows; ++i)
+            {
+                xpos = (ClientRectangle.Width - imgWidth * blocksCol) / 2;
+                for (int j = 0; j < blocksCol; ++j)
+                {
+                    if (Blocks[i, j] != null)
+                        g.DrawImage(Blocks[i, j], xpos, ypos);
+                    xpos += imgWidth;
+                }
+                ypos += imgHeighth;
+            }    
+        }
+
+        private void BreakoutForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Escape:
+                    //pause
+                    ShowMenu(!pnlMenu.Visible);
+                    if(!IsPaused())
+                        PauseGame();
+                    break;
+                case Keys.Q:
+                    //quit
+                    Close();
+                    break;
+                case Keys.Left:
+                    //move paddle left
+                    if (!IsPaused())
+                        MovePaddle(picPaddle.Left - paddleSpeed);
+                    break;
+                case Keys.Right:
+                    if (!IsPaused())
+                        MovePaddle(picPaddle.Left + paddleSpeed);
+                    break;
+                    
+            }
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            //reset ball speed in new game
+            ballSpeed = 5;
+            //score = 0
+            score = 0;
+            lblScore.Text = score.ToString("D5");
+            //center game paddle
+            MovePaddle((ClientRectangle.Width - picPaddle.Width) / 2);
+            //center ball
+            picBall.Left = (ClientRectangle.Width - picBall.Width) / 2;
+            picBall.Top = 250;
+            //blocks
+            blocksCount = BuildBlocks(rand.Next(3, 8), imageList1.Images.Count);
+            //countdown
+            lblCountdown.Text = "3";
+            lblCountdown.Visible = true;
+            ShowMenu(false);
+            for (int i = 3; i > 0; --i)
+            {
+                lblCountdown.Text = i.ToString();
+                Application.DoEvents();
+                countdownSound.Play();
+                System.Threading.Thread.Sleep(1000);
+            }
+            lblCountdown.Visible = false;
+            //game will start paused
+            PauseGame(false);
+        }
+
+        private void btnQuitGame_Click(object sender, EventArgs e)
+        {
+            quitSound.Play();
+            this.Close();
+        }
+
+        private void btnResme_Click(object sender, EventArgs e)
+        {
+            PauseGame(false);
+        }
+
+    }
+}
